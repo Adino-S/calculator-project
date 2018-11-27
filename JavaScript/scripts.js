c8c9e27c23520d455d90f7f25a675cb41fb90263
Sharon Olago <adino@lisheliving.com>
2018-11-27 10:43:16 +0300

    division added too

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,8 +1,8 @@
-var multiply= function(number1,number2){
-  return number1 * number2;
+var divide= function(number1,number2){
+  return number1 / number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var results = multiply(number1, number2);
+var results = divide (number1, number2);
 alert(results);
