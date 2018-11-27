db572d3e58c69d9ee3e27b993ce671612b902837
Sharon Olago <adino@lisheliving.com>
2018-11-27 10:06:07 +0300

    Draft calculator with style and backend

---
+++ JavaScript/scripts.js
@@ -0,0 +1,17 @@
+var add= function(number1, number2){
+return number1+number2;
+};
+
+var number1= parseInt(prompt("Enter a number:"));
+var number2= parseInt(prompt("Enter another number:"));
+var results= (add(number1, number2));
+alert(result);
+
+var subtract= function(number1, number2){
+  return number1-number2;
+};
+
+var number1 = prompt("Enter a number:");
+var number2 = prompt("Enter another number:");
+var results=(subtract(number1, number));
+alert(results;
