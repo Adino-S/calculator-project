2db948ce864c0ee04309ecafb306e553dd03ad68
Sharon Olago <adino@lisheliving.com>
2018-11-27 10:41:41 +0300

    multiplication added

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,17 +1,8 @@
-var add= function(number1, number2){
-return number1+number2;
+var multiply= function(number1,number2){
+  return number1 * number2;
 };
 
-var number1= parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"));
-var results= (add(number1, number2));
-alert(result);
-
-var subtract= function(number1, number2){
-  return number1-number2;
-};
-
-var number1 = prompt("Enter a number:");
-var number2 = prompt("Enter another number:");
-var results=(subtract(number1, number));
-alert(results;
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var results = multiply(number1, number2);
+alert(results);
